7466b28c130ed82a9a0fe02c39121670440afa9e
kadencerleigh <kadence.r.leigh@gmail.com>
2026-09-07 23:10:49 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="enchanted-bookshop-v4-7-13-1-safe-physical-stacks";
+const CACHE="enchanted-bookshop-v4-7-14-mobile-bookshop";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
