ea501503e453ef3027c1683f684e978e997b5e33
kadencerleigh <kadence.r.leigh@gmail.com>
2026-09-09 15:26:49 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ const CACHE="enchanted-bookshop-v4-23-bookshop-atmospheres";
 const CORE=[
  "./",
  "./index.html",
- "./styles.css?v=4.26.7",
- "./app.js?v=4.26.7",
- "./manifest.json?v=4.26.7",
- "./icon-192.png?v=4.26.7",
+ "./styles.css?v=4.26.8",
+ "./app.js?v=4.26.8",
+ "./manifest.json?v=4.26.8",
+ "./icon-192.png?v=4.26.8",
  "./icon-512.png"
 ];
 
